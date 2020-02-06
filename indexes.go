0cd00e094da2b6f8806748e258f54d2d546525c9
Behruz Valiev <behruzvaliev@gmail.com>
2020-02-06 15:07:23 +0500

    fix: fixes #2

--- indexes.go
+++ indexes.go
@@ -1,7 +1,7 @@
 package main
 
 // Index ...
-func Index(slice []int, predicate func(int) bool) int {
+func Index(slice []House, predicate func(House) bool) int {
 	for i, elem := range slice {
 		if predicate(elem) {
 			return i
@@ -11,33 +11,29 @@ func Index(slice []int, predicate func(int) bool) int {
 }
 
 // All ...
-func All(slice []int, predicate func(int) bool) bool {
-	return Index(slice, func(i int) bool {
-		return !predicate(i)
+func All(slice []House, predicate func(House) bool) bool {
+	return Index(slice, func(h House) bool {
+		return !predicate(h)
 	}) == -1
 }
 
 // Any ...
-func Any(slice []int, predicate func(int) bool) bool {
+func Any(slice []House, predicate func(House) bool) bool {
 	return Index(slice, predicate) != -1
 }
 
 // None ...
-func None(slice []int, predicate func(int) bool) bool {
+func None(slice []House, predicate func(House) bool) bool {
 	return Index(slice, predicate) == -1
 }
 
-// Find ...
-func Find(slice []int, predicate func(int) bool) int {
+// Finds ...
+func Finds(slice []House, predicate func(House) bool) House {
 	result := Index(slice, predicate)
 	if result == -1 {
-		return 0
+		return House{}
 	}
 	return slice[result]
 }
 
 func main() {}
-
-func nulls(i int) bool {
-	return true
-}
--- indexes_test.go
+++ indexes_test.go
@@ -7,27 +7,33 @@ import (
 func TestAny(t *testing.T) {
 	tests := []struct {
 		name      string
-		datas     []int
-		predicate func(int) bool
+		datas     []House
+		predicate func(House) bool
 		want      bool
 	}{
-		{"Testing any is true", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 93 || i == 14 || i == 23 {
-				return true
-			}
-			return false
-		}, true},
-		{"Testing any return false", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 11 {
-				return true
-			}
-			return false
-		}, false},
+		{name: "Testing any return true",
+			datas: houses,
+			predicate: func(h House) bool {
+				if h.Name == "Center1" || h.Name == "Center2" || h.Name == "Center3" {
+					return true
+				}
+				return false
+			},
+			want: true},
+		{name: "Testing any return false",
+			datas: houses,
+			predicate: func(h House) bool {
+				if h.Name == "Center5" {
+					return true
+				}
+				return false
+			},
+			want: false},
 	}
 
 	for _, tt := range tests {
 		if got := Any(tt.datas, tt.predicate); got != tt.want {
-			t.Errorf("Name: %s TestAny() got: %v but want %v", tt.name, got, tt.want)
+			t.Errorf("Name: %s got: %v but want %v", tt.name, got, tt.want)
 		}
 	}
 }
@@ -35,18 +41,18 @@ func TestAny(t *testing.T) {
 func TestNone(t *testing.T) {
 	tests := []struct {
 		name      string
-		datas     []int
-		predicate func(int) bool
+		datas     []House
+		predicate func(House) bool
 		want      bool
 	}{
-		{"TestNone must found item", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 86 {
+		{"TestNone must found item", houses, func(h House) bool {
+			if h.Name == "Center1" {
 				return true
 			}
 			return false
 		}, false},
-		{"TestNone don't found item", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 22 {
+		{"TestNone don't found item", houses, func(h House) bool {
+			if h.Name == "Center6" {
 				return true
 			}
 			return false
@@ -63,18 +69,18 @@ func TestNone(t *testing.T) {
 func TestIndex(t *testing.T) {
 	tests := []struct {
 		name      string
-		datas     []int
-		predicate func(int) bool
+		datas     []House
+		predicate func(House) bool
 		want      int
 	}{
-		{"TestIndex must found 86 in index 2", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 86 {
+		{"TestIndex must found Center4", houses, func(h House) bool {
+			if h.Name == "Center4" {
 				return true
 			}
 			return false
-		}, 2},
-		{"TestIndex must return -1", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 22 {
+		}, 3},
+		{"TestIndex must return -1", houses, func(h House) bool {
+			if h.Name == "Center5" {
 				return true
 			}
 			return false
@@ -88,31 +94,55 @@ func TestIndex(t *testing.T) {
 	}
 }
 
+type House struct {
+	Name  string
+	Price int64
+}
+
+var houses = []House{
+	{Name: "Center1", Price: 200_000},
+	{Name: "Center2", Price: 250_000},
+	{Name: "Center3", Price: 300_000},
+	{Name: "Center4", Price: 350_000},
+}
+
 func TestFind(t *testing.T) {
+	want := House{Name: "Center1", Price: 200_000}
+	notFind := House{}
+
 	tests := []struct {
 		name      string
-		datas     []int
-		predicate func(int) bool
-		want      int
+		datas     []House
+		predicate func(House) bool
+		want      House
 	}{
-		{"TestFind must return 14", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 14 {
+		{
+			name:  "Test Find must return",
+			datas: houses,
+			predicate: func(h House) bool {
+				if h == want {
+					return true
+				}
+				return false
+			},
+			want: House{Name: "Center1", Price: 200_000},
+		},
+		{
+			name:  "Test Find must return 2",
+			datas: houses,
+			predicate: func(h House) bool {
+				if h != notFind {
+					return false
+				}
 				return true
-			}
-			return false
-		}, 14},
-
-		{"TestFind must not find element and return 0", []int{23, 14, 86, 67, 93}, func(i int) bool {
-			if i == 95 {
-				return true
-			}
-			return false
-		}, 0},
+			},
+			want: notFind,
+		},
 	}
 
-	for _, tt := range tests {
-		if got := Find(tt.datas, tt.predicate); got != tt.want {
-			t.Errorf("Name: %s TestFind() got: %v and want %v", tt.name, got, tt.want)
+	for _, test := range tests {
+		if got := Finds(test.datas, test.predicate); got != test.want {
+			t.Errorf("Name: %s got: %v and want %v", test.name, got, test.want)
 		}
 	}
 }
@@ -120,18 +150,20 @@ func TestFind(t *testing.T) {
 func TestAll(t *testing.T) {
 	tests := []struct {
 		name      string
-		datas     []int
-		predicate func(int) bool
+		datas     []House
+		predicate func(House) bool
 		want      bool
 	}{
-		{"Testing all, false ", []int{21, 33, 45, 12, 93, 87}, func(i int) bool {
-			if i == 11 {
+		{"Testing all, false ", houses, func(h House) bool {
+			if h.Name == "Center6" {
 				return true
 			}
 			return false
 		}, false},
-		{"Testing all, true", []int{93, 93, 24, 93, 93}, func(i int) bool {
-			if i == 93 || i == 24 {
+		{"Testing all, true", houses, func(h House) bool {
+			if h.Name == "Center1" || h.Name == "Center2" {
+				return true
+			} else if h.Name == "Center3" || h.Name == "Center4" {
 				return true
 			}
 			return false
@@ -140,7 +172,7 @@ func TestAll(t *testing.T) {
 
 	for _, tt := range tests {
 		if got := All(tt.datas, tt.predicate); got != tt.want {
-			t.Errorf("Name: %s, TestAll() got: %v and want %v", tt.name, got, tt.want)
+			t.Errorf("Name: %s got: %v and want %v", tt.name, got, tt.want)
 		}
 	}
 }
